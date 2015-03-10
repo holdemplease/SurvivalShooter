11a2a9295db692471e6c65d3dc57ae68e216ba8e
Jeremy <holdemplease@hotmail.com>
2015-03-11 01:15:01 +0900

    completes Phase 10: Game Over and entire tutorial game!

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -3,10 +3,10 @@
 public class GameOverManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
-
+	public float restartDelay;
 
     Animator anim;
-
+	float restartTimer;
 
     void Awake()
     {
@@ -19,6 +19,13 @@ public class GameOverManager : MonoBehaviour
         if (playerHealth.currentHealth <= 0)
         {
             anim.SetTrigger("GameOver");
+
+			restartTimer += Time.deltaTime;
+
+			if(restartTimer >= restartDelay)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
         }
     }
 }
