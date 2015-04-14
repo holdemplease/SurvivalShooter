743242543a826add466c1df6f7a0b91853dd3e41
Jeremy <holdemplease@hotmail.com>
2015-04-14 14:56:41 +0900

    implement hittable barrel and explode it creating area damage

---
+++ Assets/Scripts/Enemy/DestructibleHealth.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+
+public class DestructibleHealth : MonoBehaviour	
+{
+	public int startingHealth = 100;
+	public int currentHealth;
+	public float sinkSpeed = 2.5f;
+	public int scoreValue = 10;
+	public AudioClip deathClip;
+	public float thrust;
+	public Rigidbody rigidBody;
+	public Transform attackerTransform;
+	public float explodeRadius = 5.0F;
+	public float explodeForce = 10.0F;
+	public int explosionDamage = 100;
+	
+	
+	//	Animator anim;
+	AudioSource enemyAudio;
+	ParticleSystem hitParticles;
+	CapsuleCollider capsuleCollider;
+	bool isDead;
+	bool isSinking;
+	bool isTakeDamage;
+	
+	void Awake ()
+	{
+		//		anim = GetComponent <Animator> ();
+		enemyAudio = GetComponent <AudioSource> ();
+		hitParticles = GetComponentInChildren <ParticleSystem> ();
+		capsuleCollider = GetComponent <CapsuleCollider> ();
+		rigidBody = GetComponent<Rigidbody>();
+		attackerTransform = GetComponent<Transform> ();
+		
+		currentHealth = startingHealth;
+	}
+	
+	
+	void Update ()
+	{
+		if(isSinking)
+		{
+			//			transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+		}
+	}
+	
+	
+	public void TakeDamage (int amount, Vector3 hitPoint)
+	{
+		if(isDead)
+			return;
+		
+		enemyAudio.Play ();
+		
+		currentHealth -= amount;
+		
+		hitParticles.transform.position = hitPoint;
+		hitParticles.Play();
+		
+		rigidBody.AddRelativeForce(attackerTransform.forward * thrust);
+		Debug.Log("The direction is "+ attackerTransform.forward);
+
+		if(currentHealth <= 0)
+		{
+			Explode ();
+		}
+	}
+	
+	
+	void Explode ()
+	{
+		isDead = true;
+		
+		//		capsuleCollider.isTrigger = true;
+		
+		//		anim.SetTrigger ("Dead");
+
+		enemyAudio.clip = deathClip;
+		enemyAudio.Play ();
+
+		inflictAreaDamage ();
+//		transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+//		GetComponent <Rigidbody> ().isKinematic = true;
+//		Destroy (gameObject, 2f);
+//		Invoke ("RemoveObject", 1);
+		// launch the can upward with the final explosion
+		rigidBody.AddForce(Vector3.up * thrust*5);
+
+		Destroy (gameObject, 2f);
+	}
+
+//	void RemoveObject()
+//	{
+////		GetComponent <Rigidbody> ().isKinematic = true;
+//
+//	}
+
+//	public void StartSinking ()
+//	{
+//		//		GetComponent <Rigidbody> ().isKinematic = true;
+//		isSinking = true;
+//		//		ScoreManager.score += scoreValue;
+//		//		Destroy (gameObject, 2f);
+//	}
+	
+	public void inflictAreaDamage ()
+	{
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere (explosionPos, explodeRadius);
+		foreach (Collider hit in colliders)
+		{
+			if (hit && hit.GetComponent<Rigidbody>())
+			{
+				hit.attachedRigidbody.AddExplosionForce (explodeForce, explosionPos, explodeRadius);
+				EnemyHealth enemyHealth = hit.GetComponent<EnemyHealth> ();
+				if(enemyHealth)
+					enemyHealth.TakeDamage (explosionDamage, hit.attachedRigidbody.transform.position);
+			}
+		}
+	}
+	
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,56 +3,56 @@
 public class PlayerMovement : MonoBehaviour
 {
 	public float speed = 6f;
-
+	
 	Vector3 movement;
 	Animator anim;
 	Rigidbody playerRigidBody;
 	int floorMask;
 	float cameraRayLength = 100f;
-
+	
 	void Awake()
 	{
 		floorMask = LayerMask.GetMask ("Floor");
 		anim = GetComponent <Animator> ();
 		playerRigidBody = GetComponent<Rigidbody> ();
-
+		
 	}
-
+	
 	void FixedUpdate()
 	{
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxisRaw ("Vertical");
-
+		
 		Move (h, v);
 		Turning ();
 		Animating (h, v);
 	}
-
+	
 	void Move(float h, float v)
 	{
 		movement.Set (h, 0f, v);
-
+		
 		movement = movement.normalized * speed * Time.deltaTime;
-
+		
 		playerRigidBody.MovePosition (transform.position + movement);
 	}
-
+	
 	void Turning()
 	{
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-
+		
 		RaycastHit floorhit;
-
+		
 		if(Physics.Raycast (camRay, out floorhit, cameraRayLength, floorMask))
 		{
 			Vector3 playerToMouse = floorhit.point - transform.position;
 			playerToMouse.y = 0f;
-
+			
 			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
 			playerRigidBody.MoveRotation(newRotation);
 		}
 	}
-
+	
 	void Animating(float h, float v)
 	{
 		bool walking = h != 0f || v != 0f;
